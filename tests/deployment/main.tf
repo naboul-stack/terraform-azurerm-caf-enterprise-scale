8cf616e46d55c99d9f79c4903a422b1cd89e5738
nabenmer <nabenmer@microsoft.com>
2021-03-04 16:48:07 +0100

    running_version

--- tests/deployment/main.tf
+++ tests/deployment/main.tf
@@ -2,11 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ""
+      version = ">= 2.31.1"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The tenant_id is used to set the root_parent_id value for the enterprise_scale module."
+  default = "69e08b40-2dfc-44cd-b3b5-e808212d7e30"
 }
--- tests/deployment/variables.tf
+++ tests/deployment/variables.tf
@@ -1,15 +1,19 @@
 variable "root_id_1" {
   type = string
+  default = "nbmrootid1"
 }
 
 variable "root_id_2" {
   type = string
+  default = "nbmrootid2"
 }
 
 variable "root_id_3" {
   type = string
+  default = "nbmrootid3"
 }
 
 variable "root_name" {
   type = string
+  default = "nbmrootname"
 }
